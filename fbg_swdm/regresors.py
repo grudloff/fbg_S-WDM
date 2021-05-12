6854d4111b2f593c0f3817b83eeaa867dc470c6e
Gabriel Rudloff <gabriel.rudloff@gmail.com>
2021-05-11 22:00:26 -0400

    reg lint update

--- fbg_swdm/regresors.py
+++ fbg_swdm/regresors.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-#already implemented regressions
+# Already implemented regressions
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
 
@@ -9,9 +9,10 @@ from lssvr import LSSVR
 
 class ELM:
     def __init__(self, input_size, hidden_size):
-    
-        self.input_weights = np.matrix(np.random.normal(size=[input_size,hidden_size]))
-    
+
+        self.input_weights = np.matrix(np.random.normal(size=[input_size,
+                                                              hidden_size]))
+
         self.biases = np.matrix(np.random.normal(size=[hidden_size]))
 
     def sigmoid(self, x):
@@ -29,7 +30,6 @@ class ELM:
         return out
 
     def fit(self, X, y, hidden_size=1000):
-        input_size = X.shape[1]
         H = self.hidden_nodes(X)
         H_moore_penrose = np.linalg.inv(H.T * H) * H.T
         self.output_weights = H_moore_penrose * y
