6a8aa08abf320b0e472941d7a2ba724b44d6a55c
Gabriel Rudloff <gabriel.rudloff@gmail.com>
2021-05-11 20:24:09 -0400

    reconstruction error visualization on plot_sweep

--- fbg_swdm/visualization.py
+++ fbg_swdm/visualization.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from fbg_swdm.variables import *
-from fbg_swdm.simulation import R
+from fbg_swdm.simulation import R, X
 
 def mae(a, b):
   return np.mean(np.abs(a-b))
@@ -16,7 +16,7 @@ def plot_dist(y, y_label=None, mean=False):
     print("mean("+y_label+") =", np.mean(y))
 
 # Plot sweep of one FBG with the other static
-def plot_sweep(model, d=0.6*n, normalize = True, N=300):
+def plot_sweep(model, d=0.6*n, normalize = True, rec_error=False,N=300):
   
   y = np.zeros([N,FBGN])
   # Static
@@ -29,19 +29,20 @@ def plot_sweep(model, d=0.6*n, normalize = True, N=300):
   
   if normalize:
     X = X/np.sum(I) # scale input
-    prediction = A0+D*model.predict(X)
+    y_hat = A0+D*model.predict(X)
+    X = X*np.sum(I)
 
   else:
-    prediction = model.predict(X)
+    y_hat = model.predict(X)
 
-  error = np.abs(y - prediction)
+  error = np.abs(y - y_hat)
 
   plot_dist(error/p, "Absolute Error [pm]", mean=True)
 
   fig = plt.figure(figsize=figsize)
 
-  a1 = fig.add_axes([0,0,1,1])
-  a1.plot(y[:,1]/n, prediction/n, linewidth=2)
+  a1 = plt.gca()
+  a1.plot(y[:,1]/n, y_hat/n, linewidth=2)
   a1.set_xlabel("$\lambda_{B_2}$ [nm]")
   a1.set_ylabel('$\lambda_{B_i}$ [nm]')
 
@@ -49,3 +50,10 @@ def plot_sweep(model, d=0.6*n, normalize = True, N=300):
   a2.plot(y[:,1]/n, np.sum(error,axis=1)/p, "--r")
   a2.set_ylabel('Absolute Error [pm]')
   fig.legend(labels = ("FBG1", "FBG2", "Absolute Error"))
+
+  if rec_error:
+    plt.figure(figsize=figsize)
+    X_hat =  np.sum(R(A[None, :, None], y_hat[:, None, :], I[None, None, :], dA[None, None, :]) ,axis=-1)
+    plt.plot(y[:,1]/n, np.sum(np.abs(X-X_hat), axis=1))
+    plt.xlabel("$\lambda_{B_2}$ [nm]")
+    plt.ylabel("$Reconstruction Error$")
