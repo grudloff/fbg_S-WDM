fix normalization of serial topology

fbg_swdm/simulation.py
@@ -131,8 +131,12 @@ def normalize(X_train, y_train, X_test, y_test):
     y_train = (y_train - vars.λ0)/vars.Δ
     y_test = (y_test - vars.λ0)/vars.Δ
 
-    X_train = X_train/np.sum(vars.A)
-    X_test = X_test/np.sum(vars.A)
+    if vars.topology == 'parallel':
+        X_train = X_train/np.sum(vars.A)
+        X_test = X_test/np.sum(vars.A)
+    if vars.topology == 'serial':
+        X_train = X_train/np.max(vars.A)
+        X_test = X_test/np.max(vars.A)        
 
     return X_train, y_train, X_test, y_test
 
@@ -140,8 +144,12 @@ def normalize(X_train, y_train, X_test, y_test):
 def denormalize(X_train, y_train, X_test, y_test):
     y_train = y_train*vars.Δ + vars.λ0
     y_test = y_test*vars.Δ + vars.λ0
+    if vars.topology == 'parallel':
+        X_train = X_train*np.sum(vars.A)
+        X_test = X_test*np.sum(vars.A)
+    if vars.topology == 'serial':
+        X_train = X_train*np.max(vars.A)
+        X_test = X_test*np.max(vars.A) 
 
-    X_train = X_train*np.sum(vars.A)
-    X_test = X_test*np.sum(vars.A)
 
     return X_train, y_train, X_test, y_test