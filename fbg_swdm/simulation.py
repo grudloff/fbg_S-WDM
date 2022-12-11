e004588fba1e18f8efce496f417750e45fe0ea18
Gabriel Rudloff <gabriel.rudloff@gmail.com>
2022-12-11 17:57:41 -0300

    vars to config

--- fbg_swdm/simulation.py
+++ fbg_swdm/simulation.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import log as ln
 from numpy import exp as exp
-import fbg_swdm.variables as vars
+import fbg_swdm.variables as config
 from math import sqrt
 from functools import wraps
 
@@ -32,26 +32,26 @@ def listify(func):
 # reflection spectrum
 def gaussian_R(λb, λ, A, Δλ, I, Δn_dc):
     if I is None:
-        I = vars.I
+        I = config.I
     if Δλ is None:
-        Δλ = vars.Δλ
+        Δλ = config.Δλ
     return A*I*exp(-4*ln(2)*((λ - λb)/Δλ)**2)
 
 def partial_R(λb, λ, Δλ, ζ, Δn_dc):
     if Δλ is None and ζ is None:
-        Δn = vars.Δn
-        κ = vars.κ
-        L = vars.L
+        Δn = config.Δn
+        κ = config.κ
+        L = config.L
     elif Δλ is None or ζ is None:
         raise ValueError("Both Δλ and ζ should be None to use precomputed parameters.")
     else:
-        S = vars.get_saturation(ζ)
-        Δn = Δλ*vars.n_eff/vars.λ0/vars.η/np.sqrt(1 + (vars.π/ζ)**2)/S
-        κ = vars.π*Δn/vars.λ0*vars.η
+        S = config.get_saturation(ζ)
+        Δn = Δλ*config.n_eff/config.λ0/config.η/np.sqrt(1 + (config.π/ζ)**2)/S
+        κ = config.π*Δn/config.λ0*config.η
         L = ζ/κ  # length
     
-    Δβ = 2*vars.π*vars.n_eff*(1/λ - 1/λb)  # Δβ = β - π/Λ
-    σ = 2*vars.π/λ*Δn_dc*vars.η
+    Δβ = 2*config.π*config.n_eff*(1/λ - 1/λb)  # Δβ = β - π/Λ
+    σ = 2*config.π/λ*Δn_dc*config.η
     σ_hat = Δβ + σ
     s_2 = κ**2 - σ_hat**2  # s**2
     s = np.lib.scimath.sqrt(s_2)
@@ -59,7 +59,7 @@ def partial_R(λb, λ, Δλ, ζ, Δn_dc):
     return s, s_2, L, κ, σ_hat
 
 def true_R(λb, λ, A, Δλ, I, Δn_dc):
-    ζ = vars.get_zeta(I)
+    ζ = config.get_zeta(I)
     s, s_2, L, κ, σ_hat = partial_R(λb, λ, Δλ, ζ, Δn_dc)
 
     # auxiliary variables
@@ -80,7 +80,7 @@ def get_I(ζ):
 #TODO: transferMatrix for gaussian simulation ?
 # TODO: accepts multiple sensors computation in parallel?
 def transferMatrix(λb, λ, Δλ, I, Δn_dc):
-    ζ = vars.get_zeta(I)
+    ζ = config.get_zeta(I)
     s, s_2, L, κ, σ_hat = partial_R(λb, λ, Δλ, ζ, Δn_dc)
     sL = s*L
     cosh_sL = np.cosh(sL)
@@ -122,7 +122,7 @@ def prep_dims(A_b, λ, A, Δλ, I, Δn_dc):
     return A_b, λ, A, Δλ, I, Δn_dc
 
 def R(*args, **kwargs):
-    simulation = kwargs.pop('simulation', vars.simulation)
+    simulation = kwargs.pop('simulation', config.simulation)
     if simulation == 'gaussian':
         return gaussian_R(*args, **kwargs)
     elif simulation == 'true':
@@ -138,24 +138,24 @@ def X(A_b, λ=None, A=None, Δλ=None, I=None, Δn_dc=None, batch_size=None, **k
 
     # TODO: move default to R functions
     # defaults if None
-    λ = vars.λ if λ is None else λ 
-    A = vars.A if A is None else A
+    λ = config.λ if λ is None else λ 
+    A = config.A if A is None else A
     # Don't set I and Δλ to default to use precomputed parameters
     # Δλ = vars.Δλ if Δλ is None else Δλ 
     # I = vars.I if I is None else I
-    Δn_dc = vars.Δn_dc if Δn_dc is None else Δn_dc 
+    Δn_dc = config.Δn_dc if Δn_dc is None else Δn_dc 
 
     if batch_size != None:
-        A_b = np.array_split(A_b, vars.M//batch_size)
+        A_b = np.array_split(A_b, config.M//batch_size)
         x = np.concatenate([X(a_b, λ, A, Δλ, I, Δn_dc) for a_b in A_b])
         return x
 
     A_b, λ, A, Δλ, I, Δn_dc = prep_dims(A_b, λ, A, Δλ, I, Δn_dc)
 
-    if vars.topology == 'parallel':
+    if config.topology == 'parallel':
         x = np.sum(R(A_b, λ, A, Δλ, I, Δn_dc, **kwargs), axis=-1)
 
-    elif vars.topology == 'serial_new':
+    elif config.topology == 'serial_new':
         """Serial topology matrix simulation, no incoherent interface between FBGs"""
 
         T_prev = np.identity(2)
@@ -172,7 +172,7 @@ def X(A_b, λ=None, A=None, Δλ=None, I=None, Δn_dc=None, batch_size=None, **k
         x = T[1,0]/T[0,0]
         x = np.abs(x)**2
 
-    elif vars.topology == 'serial_rand':
+    elif config.topology == 'serial_rand':
         """serial_new but with incoherent interface by simulation of random multiple paths"""
         
         L = 10000 # batch_size
@@ -187,8 +187,8 @@ def X(A_b, λ=None, A=None, Δλ=None, I=None, Δn_dc=None, batch_size=None, **k
             T_prev = batch_matmul(T_prev, T[..., None])
 
             # random gap phase change
-            if i < vars.Q:
-                F = 1j*vars.π*2*np.random.rand(L)[None, :]*np.array([-1, 1])[:, None]
+            if i < config.Q:
+                F = 1j*config.π*2*np.random.rand(L)[None, :]*np.array([-1, 1])[:, None]
                 F = np.exp(F)
                 F = np.identity(2)[..., None]*F[None,...]
                 T_prev = batch_matmul(T_prev, F[:,:,None])
@@ -201,13 +201,13 @@ def X(A_b, λ=None, A=None, Δλ=None, I=None, Δn_dc=None, batch_size=None, **k
         x = np.mean(x, axis=-1) # average different paths
 
 
-    elif vars.topology == 'serial_phase':
+    elif config.topology == 'serial_phase':
         """serial_rand equivalent but with a phase sweep instead, making it more efficient"""
         
-        N=vars.φN
+        N=config.φN
         phase = np.arange(0,1,1.0/N)
         #phase = np.linspace(1/N/2,1- 1/N/2, N)
-        phase = 1j*vars.π*phase
+        phase = 1j*config.π*phase
         F = phase[None, :]*np.array([-1, 1])[:, None]
         F = np.exp(F)
         F = np.identity(2)[..., None]*F[None,...]
@@ -223,7 +223,7 @@ def X(A_b, λ=None, A=None, Δλ=None, I=None, Δn_dc=None, batch_size=None, **k
             T_prev = batch_matmul(T_prev, T[..., None])
 
             # scan gap phase change
-            if i < vars.Q:
+            if i < config.Q:
                 T_prev = batch_matmul_outprod(T_prev, F[:,:,None,None])
                 # flatten outer product
                 T_prev = T_prev.reshape(*T_prev.shape[:-2], -1)
@@ -235,12 +235,12 @@ def X(A_b, λ=None, A=None, Δλ=None, I=None, Δn_dc=None, batch_size=None, **k
         x = np.abs(x)**2
         x = np.mean(x, axis=-1) # average different paths
 
-    elif vars.topology == 'serial_abs':
+    elif config.topology == 'serial_abs':
         """Approximation of serial_phase"""
         
         T_prev = np.identity(2)
 
-        for a, r_next in zip(A.T, np.rollaxis(R(A_b, λ, np.ones(vars.Q), 
+        for a, r_next in zip(A.T, np.rollaxis(R(A_b, λ, np.ones(config.Q), 
                                               Δλ, I, Δn_dc, **kwargs), -1)):
             t = 1-r
 
@@ -258,17 +258,17 @@ def X(A_b, λ=None, A=None, Δλ=None, I=None, Δn_dc=None, batch_size=None, **k
         T = T_prev
         x = T[1,0]/T[0,0]
 
-    elif vars.topology == 'serial':
+    elif config.topology == 'serial':
         """Old, only valid for 2-FBGs"""
         x = 0
         for x_next in np.rollaxis(R(A_b, λ, A, Δλ, I, Δn_dc, **kwargs), -1):
             x = x + (1-x)**2*x_next/(1-x_next*x)
 
-    elif vars.topology == 'serial_rec':
+    elif config.topology == 'serial_rec':
         """Recurrent equivalent of serial_abs """
         r = 0
         t = 1
-        for a, r_next in zip(A.T, np.rollaxis(R(A_b, λ, np.ones(vars.Q), 
+        for a, r_next in zip(A.T, np.rollaxis(R(A_b, λ, np.ones(config.Q), 
                                               Δλ, I, Δn_dc, **kwargs), -1)):
             t_next = 1 - r_next
             F = 1/(1 - a*r*r_next) # resonance
@@ -287,23 +287,23 @@ def X(A_b, λ=None, A=None, Δλ=None, I=None, Δn_dc=None, batch_size=None, **k
 # gen M datapoints on an N-sized spectrum
 def gen_data(train_dist="uniform", batch_size=None):
 
-    if vars.topology=='parallel' and np.sum(vars.A)>1:
+    if config.topology=='parallel' and np.sum(config.A)>1:
         raise ValueError('Attenuation vector should sum less than one in the parallel topology.')
 
-    Δ = vars.portion*vars.Δ
+    Δ = config.portion*config.Δ
 
     if train_dist == "mesh":
-        y_train = np.linspace(vars.λ0-Δ, vars.λ0+Δ,
-                              int(np.ceil(vars.M**(1/vars.Q))))  # 1d array
-        aux = [y_train for _ in range(vars.Q)]
+        y_train = np.linspace(config.λ0-Δ, config.λ0+Δ,
+                              int(np.ceil(config.M**(1/config.Q))))  # 1d array
+        aux = [y_train for _ in range(config.Q)]
         y_train = np.meshgrid(*aux)  # 2d mesh from that array
-        y_train = np.reshape(y_train, (vars.Q, vars.M)).T
+        y_train = np.reshape(y_train, (config.Q, config.M)).T
 
     elif train_dist == "uniform":
-        y_train = vars.λ0 + np.random.uniform(-Δ, Δ, [vars.M, vars.Q])
+        y_train = config.λ0 + np.random.uniform(-Δ, Δ, [config.M, config.Q])
 
-    m = int(0.1*vars.M)
-    y_test = vars.λ0 + np.random.uniform(-Δ, Δ, [m, vars.Q])
+    m = int(0.1*config.M)
+    y_test = config.λ0 + np.random.uniform(-Δ, Δ, [m, config.Q])
 
     # broadcast shape: N, M, FBGN
     X_train = X(y_train, batch_size=batch_size)
@@ -316,13 +316,13 @@ def gen_data(train_dist="uniform", batch_size=None):
 # ---------------------------------------------------------------------------- #
 
 @listify
-def normalize(X=None, y=None, *args):
+def normalize(X=None, y=None, *args, **kwargs):
     if X is not None:
         # x is not normalized
         yield X
 
     if y is not None:
-        y = (y - vars.λ0)/vars.Δ
+        y = (y - config.λ0)/config.Δ
         yield y    
 
     if args:
@@ -335,7 +335,7 @@ def denormalize(X=None, y=None, *args):
         yield X
 
     if y is not None:
-        y = y*vars.Δ + vars.λ0
+        y = y*config.Δ + config.λ0
         yield y    
         
     if args:
