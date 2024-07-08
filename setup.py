75028e6213514a411acb3a4d193356b04ed8f8de
Gabriel Rudloff <gabriel.rudloff@gmail.com>
2024-07-08 19:07:25 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='fbg_swdm',
+    author="Gabriel Rudloff",
+    author_email="gabriel.rudloff@gmail.com",
+    version='1.0',
+    packages=find_packages(),
+    install_requires=["numpy", "matplotlib", "scikit-learn", "pandas",
+                      "leap_ec", "lssvr @ git+https://github.com/grudloff/lssvr",
+                      "scipy", "torch", "pytorch-lightning", "ofiber"],
+    include_package_data=True
+)
